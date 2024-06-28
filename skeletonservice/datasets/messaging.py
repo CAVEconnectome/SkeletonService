3411cc6edf1040f44e5b1dc87e5cc71d9ab05abe
Keith Wiley <keith.wiley@alleninstitute.org>
2024-06-28 09:16:33 -0700

    skeleton messaging dev

--- skeletonservice/datasets/messaging.py
+++ skeletonservice/datasets/messaging.py
@@ -4,19 +4,18 @@ from .service import SkeletonService
 
 def callback(payload):
     print("Skeleton Cache received message: ", payload)
-    skeleton_params = {
-        "datastack_name":      payload.attributes["skeleton_params_datastack_name"],
-        "rid":                 int(payload.attributes["skeleton_params_rid"]),
-        "materialize_version": int(payload.attributes["skeleton_params_materialize_version"]),
-        "output_format":       payload.attributes["skeleton_params_output_format"],
-        "sid":                 int(payload.attributes["skeleton_params_sid"]),
-        "bucket":              payload.attributes["skeleton_params_bucket"],
-        "root_resolution":     [int(v) for v in payload.attributes["skeleton_params_root_resolution"].split()],
-        "collapse_soma":       False if payload.attributes["skeleton_params_collapse_soma"].lower() in ["false", "f", "0"] else True,
-        "collapse_radius":     int(payload.attributes["skeleton_params_collapse_radius"]),
-    }
+    skeleton_params = [
+        int(payload.attributes["skeleton_params_rid"]),
+        payload.attributes["skeleton_params_bucket"],
+        payload.attributes["skeleton_params_datastack_name"],
+        int(payload.attributes["skeleton_params_materialize_version"]),
+        [int(v) for v in payload.attributes["skeleton_params_root_resolution"].split()],
+        False if payload.attributes["skeleton_params_collapse_soma"].lower() in ["false", "f", "0"] else True,
+        int(payload.attributes["skeleton_params_collapse_radius"]),
+        
+    ]
     print("Skeleton Cache message parameters: ", skeleton_params)
-    skel = SkeletonService.generate_skeleton(skeleton_params)
+    skel = SkeletonService.generate_skeleton(*skeleton_params)
 
 c = MessagingClient()
 l2cache_update_queue = getenv("SKELETON_CACHE_RETRIEVE_QUEUE", "does-not-exist")
