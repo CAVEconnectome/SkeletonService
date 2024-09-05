eed782c570c81b96267763e546a88ab79b915bee
Forrest Collman <forrest.collman@gmail.com>
2024-09-05 02:10:52 -0700

    Bump version: 0.1.56 → 0.1.57

--- skeletonservice/datasets/controllers.py
+++ skeletonservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
-__version__ = "0.1.56"
+__version__ = "0.1.57"
 
 
 @mod_datasets.route("/")
--- skeletonservice/datasets/views.py
+++ skeletonservice/datasets/views.py
@@ -13,6 +13,6 @@ import flask
 import os
 import numpy as np
 
-__version__ = "0.1.56"
+__version__ = "0.1.57"
 
 views_bp = Blueprint("skeletons", __name__, url_prefix="/skeletons")
