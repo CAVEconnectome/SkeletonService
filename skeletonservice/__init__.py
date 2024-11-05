75e8673a46cc2db0085b46b5481a78104945fe03
Keith Wiley <keith.wiley@alleninstitute.org>
2024-11-05 10:06:24 -0800

    Bump version: 0.5.12 → 0.5.13

--- skeletonservice/__init__.py
+++ skeletonservice/__init__.py
@@ -16,7 +16,7 @@ from werkzeug.routing import BaseConverter
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "0.5.12"
+__version__ = "0.5.13"
 
 # migrate = Migrate()
 
