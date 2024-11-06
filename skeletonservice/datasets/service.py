7df63f8f2efdeba4afb01e4004d8c95b48495751
Keith Wiley <keith.wiley@alleninstitute.org>
2024-11-06 13:29:24 -0800

    Dynamic worker count detection

--- skeletonservice/datasets/service.py
+++ skeletonservice/datasets/service.py
@@ -1516,6 +1516,9 @@ class SkeletonService:
             print(f"Message has been dispatched to {exchange}: {datastack_name} {rid} skvn:{skeleton_version} {bucket}")
 
         skeleton_generation_time_estimate_secs = 60  # seconds
-        num_workers = 15  # Number of skeleton worker (Kubernetes pods) available (# This should be read from the server somehow)
+        num_workers = os.environ.get("SKELETONCACHE_MAX_REPLICAS", 15)  # Number of skeleton worker (Kubernetes pods) available (# This should be read from the server somehow)
         estimated_async_time_secs_upper_bound =  math.ceil(len(rids) / num_workers) * skeleton_generation_time_estimate_secs
+        if verbose_level >= 1:
+            print(f"SKELETONCACHE_MAX_REPLICAS in environment: {'SKELETONCACHE_MAX_REPLICAS' in os.environ}")
+            print(f"Estimated async time: ceiling({len(rids)} / {num_workers}) * {skeleton_generation_time_estimate_secs} = {estimated_async_time_secs_upper_bound}")
         return estimated_async_time_secs_upper_bound
