4779a6ecf7e3e0c1ad73131bc307f74001b772ff
Forrest Collman <forrest.collman@gmail.com>
2025-03-09 11:18:48 -0700

    Bump version: 0.17.2 → 0.17.3

--- skeletonservice/__init__.py
+++ skeletonservice/__init__.py
@@ -16,7 +16,7 @@ from werkzeug.routing import BaseConverter
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "0.17.2"
+__version__ = "0.17.3"
 
 # migrate = Migrate()
 
--- skeletonservice/datasets/api.py
+++ skeletonservice/datasets/api.py
@@ -17,7 +17,7 @@ from middle_auth_client import (
 
 from typing import List, Dict
 
-__version__ = "0.17.2"
+__version__ = "0.17.3"
 
 
 authorizations = {
