4f4b65b1c39463b3dd6fb9955b4904231ade29ec
Keith Wiley <keith.wiley@alleninstitute.org>
2025-03-19 16:35:52 -0700

    Messaging bug fix.

--- skeletonservice/datasets/messaging.py
+++ skeletonservice/datasets/messaging.py
@@ -68,10 +68,9 @@ def callback(payload):
 c = MessagingClient()
 l2cache_low_priority_update_queue = getenv("SKELETON_CACHE_LOW_PRIORITY_RETRIEVE_QUEUE", "does-not-exist")
 l2cache_high_priority_update_queue = getenv("SKELETON_CACHE_HIGH_PRIORITY_RETRIEVE_QUEUE", "does-not-exist")
-l2cache_low_dead_update_queue = getenv("SKELETON_CACHE_LOW_DEAD_LETTER_RETRIEVE_QUEUE", "does-not-exist")
-l2cache_high_dead_update_queue = getenv("SKELETON_CACHE_HIGH_DEAD_LETTER_RETRIEVE_QUEUE", "does-not-exist")
+l2cache_low_dead_update_queue = getenv("SKELETON_CACHE_DEAD_LETTER_RETRIEVE_QUEUE", "does-not-exist")
 c.consume_multiple([l2cache_low_priority_update_queue,
                     l2cache_high_priority_update_queue,
                     l2cache_low_dead_update_queue,
-                    l2cache_high_dead_update_queue],
+                    l2cache_low_dead_update_queue],
                     callback)
