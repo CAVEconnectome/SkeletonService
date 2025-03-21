d50a671886a8a6287a84bcb7239cfe9b05893b5f
Keith Wiley <keith.wiley@alleninstitute.org>
2025-03-21 13:27:45 -0700

    Bump version: 0.17.24 → 0.17.25

--- skeletonservice/datasets/api.py
+++ skeletonservice/datasets/api.py
@@ -17,7 +17,7 @@ from middle_auth_client import (
 
 from typing import List, Dict
 
-__version__ = "0.17.24"
+__version__ = "0.17.25"
 
 
 authorizations = {
--- skeletonservice/datasets/controllers.py
+++ skeletonservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
-__version__ = "0.17.24"
+__version__ = "0.17.25"
 
 
 @mod_datasets.route("/")
