bf64530c6be1f6eb0c651f7b760ecf72af6e1339
Keith Wiley <keith.wiley@alleninstitute.org>
2025-03-31 11:40:34 -0700

    Integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -18,7 +18,23 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 class TestSkeletonsServiceIntegration:
-    def run_test(self, server_address, fast_run=False):
+    def test_passed(self):
+        print("TEST PASSED")
+
+    def test_failed(self):
+        print("TEST FAILED")
+
+    def print_test_result(self, result):
+        if result:
+            self.test_passed()
+        else:
+            self.test_failed()
+            
+    def run_one_test(self, result):
+        self.print_test_result(result)
+        assert result
+
+    def run_one_server_test(self, server_address, fast_run=False):
         # Set things up
         self.fast_run = fast_run
 
@@ -94,15 +110,14 @@ class TestSkeletonsServiceIntegration:
     def run_test_metadata_1(self):
         precomputed_skeleton_info = self.skclient.get_precomputed_skeleton_info(skvn=self.skvn)
         # print(precomputed_skeleton_info)
-        assert precomputed_skeleton_info == {
+        self.run_one_test(precomputed_skeleton_info == {
             '@type': 'neuroglancer_skeletons',
             'transform': [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
             'vertex_attributes': [
                 {'id': 'radius', 'data_type': 'float32', 'num_components': 1},
                 {'id': 'compartment', 'data_type': 'uint8', 'num_components': 1}
             ]
-        }
-        print("TEST PASSED")
+        })
 
     # Cache status tests
 
@@ -110,32 +125,29 @@ class TestSkeletonsServiceIntegration:
         rids_exist = self.skclient.skeletons_exist(skeleton_version=self.skvn, root_ids=self.bulk_rids)
         # print(json.dumps(rids_exist, indent=4))
         if not self.fast_run:
-            assert rids_exist == {
+            self.run_one_test(rids_exist == {
                 self.bulk_rids[0]: False,
                 self.bulk_rids[1]: False
-            }
-        print("TEST PASSED")
+            })
 
     def run_test_cache_status_1_2(self):
         rids_exist = self.skclient.skeletons_exist(skeleton_version=self.skvn, root_ids=self.bulk_rids, log_warning=False)
         # print(json.dumps(rids_exist, indent=4))
         if not self.fast_run:
-            assert rids_exist == {
+            self.run_one_test(rids_exist == {
                 self.bulk_rids[0]: False,
                 self.bulk_rids[1]: False
-            }
-        print("TEST PASSED")
+            })
 
     def run_test_cache_status_1_3(self):
         # Requires CAVEclient version >= v7.6.1
         rids_exist = self.skclient.skeletons_exist(skeleton_version=self.skvn, root_ids=self.bulk_rids, verbose_level=1)
         print(json.dumps(rids_exist, indent=4))
         if not self.fast_run:
-            assert rids_exist == {
+            self.run_one_test(rids_exist == {
                 self.bulk_rids[0]: False,
                 self.bulk_rids[1]: False
-            }
-        print("TEST PASSED")
+            })
 
     # Cache contents tests
 
@@ -143,78 +155,72 @@ class TestSkeletonsServiceIntegration:
         cache_contents = self.skclient.get_cache_contents(skeleton_version=self.skvn, root_id_prefixes=self.bulk_rids)
         print(json.dumps(cache_contents, indent=4))
         if not self.fast_run:
-            assert cache_contents == {
+            self.run_one_test(cache_contents == {
                 "num_found": 0,
                 "files": []
-            }
-        print("TEST PASSED")
+            })
 
     def run_test_cache_contents_2(self):
         cache_contents = self.skclient.get_cache_contents(skeleton_version=self.skvn, root_id_prefixes=self.bulk_rids, log_warning=False)
         print(json.dumps(cache_contents, indent=4))
         if not self.fast_run:
-            assert cache_contents == {
+            self.run_one_test(cache_contents == {
                 "num_found": 0,
                 "files": []
-            }
-        print("TEST PASSED")
+            })
 
     def run_test_cache_contents_3(self):
         # Requires CAVEclient version >= v7.6.1
         cache_contents = self.skclient.get_cache_contents(skeleton_version=self.skvn, root_id_prefixes=self.bulk_rids, verbose_level=1)
         print(json.dumps(cache_contents, indent=4))
         if not self.fast_run:
-            assert cache_contents == {
+            self.run_one_test(cache_contents == {
                 "num_found": 0,
                 "files": []
-            }
-        print("TEST PASSED")
+            })
 
     # Invalid skeleton request tests
 
     def run_test_invalid_request_1(self):
         try:
             sk = self.skclient.get_skeleton(self.sample_refusal_list_rid, self.datastack_name, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
+            self.test_failed()
         except requests.HTTPError as e:
             print(e)
-            assert e.response.text == '{\n    "Error": "Problematic root id: ' + str(self.sample_refusal_list_rid) + ' is in the refusal list"\n}\n'
-        print("TEST PASSED")
+            self.run_one_test(e.response.text == '{\n    "Error": "Problematic root id: ' + str(self.sample_refusal_list_rid) + ' is in the refusal list"\n}\n')
 
     def run_test_invalid_request_2(self):
         try:
             sk = self.skclient.get_skeleton(self.sample_invalid_node_rid, self.datastack_name, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
+            self.test_failed()
         except requests.HTTPError as e:
             print(e)
-            assert e.response.text == '{\n    "Error": "Invalid root id: ' + str(self.sample_invalid_node_rid) + ' (perhaps it doesn\'t exist; the error is unclear)"\n}\n'
-        print("TEST PASSED")
+            self.run_one_test(e.response.text == '{\n    "Error": "Invalid root id: ' + str(self.sample_invalid_node_rid) + ' (perhaps it doesn\'t exist; the error is unclear)"\n}\n')
 
     def run_test_invalid_request_3(self):
         try:
             sk = self.skclient.get_skeleton(self.sample_supervoxel_rid, self.datastack_name, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
+            self.test_failed()
         except requests.HTTPError as e:
             print(e)
-            assert e.response.text == '{\n    "Error": "Invalid root id: ' + str(self.sample_supervoxel_rid) + ' (perhaps this is an id corresponding to a different level of the PCG, e.g., a supervoxel id)"\n}\n'
-        print("TEST PASSED")
+            self.run_one_test(e.response.text == '{\n    "Error": "Invalid root id: ' + str(self.sample_supervoxel_rid) + ' (perhaps this is an id corresponding to a different level of the PCG, e.g., a supervoxel id)"\n}\n')
 
     # Skeleton request tests
 
     def run_test_retrieval_1(self):
         sk = self.skclient.get_skeleton(self.single_rid, self.datastack_name, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
         # display(sk)
-        assert sk is not None and isinstance(sk, dict)
-        print("TEST PASSED")
+        self.run_one_test(sk is not None and isinstance(sk, dict))
 
     def run_test_retrieval_2(self):
         sk = self.skclient.get_skeleton(self.single_rid, self.datastack_name, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
         # display(sk)
-        assert sk is not None and isinstance(sk, dict)
-        print("TEST PASSED")
+        self.run_one_test(sk is not None and isinstance(sk, dict))
 
     def run_test_retrieval_3(self):
         sk = self.skclient.get_skeleton(self.single_rid, self.datastack_name, skeleton_version=self.skvn, output_format='swc', verbose_level=1)
         # display(sk)
-        assert sk is not None and isinstance(sk, pd.DataFrame)
-        print("TEST PASSED")
+        self.run_one_test(sk is not None and isinstance(sk, pd.DataFrame))
 
     ## Inspect the cache after generating new skeletons
 
@@ -222,47 +228,42 @@ class TestSkeletonsServiceIntegration:
         rids_exist = self.skclient.skeletons_exist(skeleton_version=self.skvn, root_ids=self.bulk_rids)
         print(json.dumps(rids_exist, indent=4))
         if not self.fast_run:
-            assert rids_exist == {
+            self.run_one_test(rids_exist == {
                 self.bulk_rids[0]: True,
                 self.bulk_rids[1]: False
-            }
-        print("TEST PASSED")
+            })
 
     def run_test_cache_status_2_2(self):
         cache_contents = self.skclient.get_cache_contents(skeleton_version=self.skvn, root_id_prefixes=self.bulk_rids)
         print(json.dumps(cache_contents, indent=4))
         if not self.fast_run:
-            assert cache_contents == {
+            self.run_one_test(cache_contents == {
                 "num_found": 1,
                 "files": [
                     f"skeleton__v4__rid-{self.bulk_rids[0]}__ds-{self.datastack_name}__res-1x1x1__cs-True__cr-7500.h5.gz"
                 ]
-            }
-        print("TEST PASSED")
+            })
 
     # Small bulk skeleton request tests
     ## This routine truncates the request list to a small number (10 at the time of this writing), returns any skeletons that are available, and submits the rest to the asynchronous queue
 
     def run_test_bulk_retrieval_1(self):
         result = self.skclient.get_bulk_skeletons(self.bulk_rids, skeleton_version=self.skvn, output_format='dict')
-        # We can't assert both root ids but only one was generated by the previous tests above.
+        # We can't test(both root ids but only one was generated by the previous tests above.
         # The other root id will be asyncronously triggered by this test but won't be available for 20-60 seconds afterwards.
-        assert(str(self.bulk_rids[0]) in result.keys())
-        print("TEST PASSED")
+        self.run_one_test(str(self.bulk_rids[0]) in result.keys())
 
     def run_test_bulk_retrieval_2(self):
         result = self.skclient.get_bulk_skeletons(self.bulk_rids, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
-        # We can't assert both root ids but only one was generated by the previous tests above.
+        # We can't test(both root ids but only one was generated by the previous tests above.
         # The other root id will be asyncronously triggered by this test but won't be available for 20-60 seconds afterwards.
-        assert(str(self.bulk_rids[0]) in result.keys())
-        print("TEST PASSED")
+        self.run_one_test(str(self.bulk_rids[0]) in result.keys())
 
     def run_test_bulk_retrieval_3(self):
         result = self.skclient.get_bulk_skeletons(self.larger_bulk_rids, skeleton_version=self.skvn, output_format='dict', verbose_level=1)
-        # We can't assert both root ids but only one was generated by the previous tests above.
+        # We can't test(both root ids but only one was generated by the previous tests above.
         # The other root id will be asyncronously triggered by this test but won't be available for 20-60 seconds afterwards.
-        assert(str(self.bulk_rids[0]) in result.keys())
-        print("TEST PASSED")
+        self.run_one_test(str(self.bulk_rids[0]) in result.keys())
 
     # Asynchronous bulk skeleton request tests
     ## This routine submits a large number of requests and returns only the estimated time to complete the job; it doesn't return any skeletons.
@@ -273,20 +274,17 @@ class TestSkeletonsServiceIntegration:
     def run_test_bulk_async_request_1(self):
         result = self.skclient.generate_bulk_skeletons_async(self.bulk_rids, skeleton_version=self.skvn)
         print(type(result), result)
-        assert result == 60.0
-        print("TEST PASSED")
+        self.run_one_test(result == 60.0)
 
     def run_test_bulk_async_request_2(self):
         result = self.skclient.generate_bulk_skeletons_async(self.bulk_rids, skeleton_version=self.skvn, verbose_level=1)
         print(type(result), result)
-        assert result == 60.0
-        print("TEST PASSED")
+        self.run_one_test(result == 60.0)
 
     def run_test_bulk_async_request_3(self):
         result = self.skclient.generate_bulk_skeletons_async(self.larger_bulk_rids, skeleton_version=self.skvn, verbose_level=1)
         print(type(result), result)
-        assert result == 60.0
-        print("TEST PASSED")
+        self.run_one_test(result == 60.0)
 
     def test_integration(self, test_app):
         # Pick a test server:
@@ -311,8 +309,8 @@ class TestSkeletonsServiceIntegration:
         
         for server_address in server_addresses:
             try:
-                assert self.run_test(server_address)#, fast_run=True)
+                self.run_one_server_test(server_address)#, fast_run=True)
             except Exception as e:
                 print(f"Error running test on {server_address}: {e}")
-                assert False
+                self.self.run_one_test(False)
         
