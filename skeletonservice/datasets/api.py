054b0b3d160dd85b7cdeb09048d06e5941cbc5d8
Keith Wiley <keith.wiley@alleninstitute.org>
2025-04-02 14:23:08 -0700

    Bump version: 0.17.40 → 0.17.41

--- skeletonservice/datasets/api.py
+++ skeletonservice/datasets/api.py
@@ -18,7 +18,7 @@ from middle_auth_client import (
 
 from typing import List, Dict
 
-__version__ = "0.17.40"
+__version__ = "0.17.41"
 
 
 authorizations = {
--- skeletonservice/datasets/views.py
+++ skeletonservice/datasets/views.py
@@ -13,6 +13,6 @@ import flask
 import os
 import numpy as np
 
-__version__ = "0.17.40"
+__version__ = "0.17.41"
 
 views_bp = Blueprint("skeletons", __name__, url_prefix="/skeletons")
