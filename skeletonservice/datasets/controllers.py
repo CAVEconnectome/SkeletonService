e965d84037e6465bf4a2535dada20ab283e052fb
Keith Wiley <keith.wiley@alleninstitute.org>
2025-04-04 09:27:24 -0700

    Bump version: 0.17.54 → 0.17.55

--- skeletonservice/datasets/controllers.py
+++ skeletonservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
-__version__ = "0.17.54"
+__version__ = "0.17.55"
 
 
 @mod_datasets.route("/")
--- skeletonservice/datasets/views.py
+++ skeletonservice/datasets/views.py
@@ -13,6 +13,6 @@ import flask
 import os
 import numpy as np
 
-__version__ = "0.17.54"
+__version__ = "0.17.55"
 
 views_bp = Blueprint("skeletons", __name__, url_prefix="/skeletons")
