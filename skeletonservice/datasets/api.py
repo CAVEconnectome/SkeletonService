cd0d5f73648dc7531f61dac6bdf226d78e598b6a
Keith Wiley <keith.wiley@alleninstitute.org>
2025-05-30 10:24:08 -0700

    Bump version: 0.21.28 → 0.21.29

--- skeletonservice/datasets/api.py
+++ skeletonservice/datasets/api.py
@@ -22,7 +22,7 @@ from middle_auth_client import (
 
 from typing import List
 
-__version__ = "0.21.28"
+__version__ = "0.21.29"
 
 google.cloud.logging.Client().setup_logging()
 
--- skeletonservice/datasets/controllers.py
+++ skeletonservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint("datasets", __name__, url_prefix="/datasets")
 
-__version__ = "0.21.28"
+__version__ = "0.21.29"
 
 
 @mod_datasets.route("/")
