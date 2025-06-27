e947ad83047a1a1299d01542b0ed54ff856f989a
Keith Wiley <keith.wiley@alleninstitute.org>
2025-06-27 10:53:55 -0700

    Bump version: 0.22.12 → 0.22.13

--- skeletonservice/__init__.py
+++ skeletonservice/__init__.py
@@ -20,7 +20,7 @@ from werkzeug.routing import BaseConverter
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "0.22.12"
+__version__ = "0.22.13"
 
 google.cloud.logging.Client().setup_logging()
 
--- skeletonservice/datasets/api.py
+++ skeletonservice/datasets/api.py
@@ -22,7 +22,7 @@ from middle_auth_client import (
 
 from typing import List
 
-__version__ = "0.22.12"
+__version__ = "0.22.13"
 
 google.cloud.logging.Client().setup_logging()
 
