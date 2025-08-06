21160b817bc182e48736cbb464f5c90eaa6e72be
Keith Wiley <keith.wiley@alleninstitute.org>
2025-08-06 16:07:51 -0700

    Debugging

--- skeletonservice/datasets/service.py
+++ skeletonservice/datasets/service.py
@@ -52,7 +52,7 @@ try:
 except Exception as e:
     print(f"Traceback for DATASTACK_NAME_REMAPPING error: {str(e)}")
     traceback.print_exc()
-    print(f"DATASTACK_NAME_REMAPPING STR: `{os.environ.get('SKELETON_DATASTACK_NAME_REMAPPING', 'Not set')}`")
+    print(f"service.py DATASTACK_NAME_REMAPPING STR: `{os.environ.get('SKELETON_DATASTACK_NAME_REMAPPING', 'Not set')}`")
     DATASTACK_NAME_REMAPPING = {}
 MESHWORK_VERSION = 1
 SKELETONIZATION_TIMES_FILENAME = "skeletonization_times_v2.csv"
