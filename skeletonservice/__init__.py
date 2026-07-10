13699bd32b5eaf4666be350d755a13cebe1d177f
Keith Wiley <keith.wiley@alleninstitute.org>
2026-07-10 11:51:36 -0700

    Bump version: 0.23.5 → 0.23.6

--- skeletonservice/__init__.py
+++ skeletonservice/__init__.py
@@ -20,7 +20,7 @@ from werkzeug.routing import BaseConverter
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "0.23.5"
+__version__ = "0.23.6"
 
 google.cloud.logging.Client().setup_logging()
 
